6660e2dc0becf124b30ef7a10f6084dee42a4d83
sunha55 <sunhapark0510@gmail.com>
2026-07-08 00:47:17 +0900

    모바일 글씨·아이콘 크기 대폭 확대

--- vite.config.js
+++ vite.config.js
@@ -43,6 +43,8 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         navigateFallback: 'index.html',
+        skipWaiting: true,
+        clientsClaim: true,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
